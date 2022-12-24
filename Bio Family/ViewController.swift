6a819f9ca605b5dc81384dc528b7dcbc3c1e51b8
kapil <kapild@solidappmaker.com>
2022-12-24 14:58:23 +0530

    Initial Commit

---
+++ Bio Family/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bio Family
+//
+//  Created by John on 24/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
